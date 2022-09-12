4affb1c4314d4458a98e60cdb262184a806854e3
dray91eu <dray91eu@gmail.com>
2022-09-11 18:16:34 -0700

    betty correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 /**
  * main - put the puts the string into a function
- * description: put pirnts \"Programming is like building a multilingual puzzle
- * returns: a value 0
+ * description: returns the value of main
+ *  pirnts \"Programming is like building a multilingual puzzle
+ * Return: a value 0
  */
 
 #include <stdio.h>
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-		return(0);
+		return (0);
 }
