f1488b35541243fc2f50c1b7e53615cacd77f24e
dray91eu <dray91eu@gmail.com>
2022-09-11 18:28:48 -0700

    this code prints with proper grammar, but the outcome is a piece of art,

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 /**
  * main - put the puts the string into a function
  * description: returns the value of main
- *  pirnts \"Programming is like building a multilingual puzzle
+ *  pirnts with proper grammar, but the outcome is a piece of art,
  * Return: a value 0
  */
 
 #include <stdio.h>
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art, ");
 		return (0);
 }
