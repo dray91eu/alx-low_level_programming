8f73803f85e349f98ce0b9240698ae5f72393c70
dray91eu <dray91eu@gmail.com>
2022-09-14 07:40:26 -0700

    this prints abc x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,14 @@
 #include "main.h"
 /**
- *print_alphabet_10x - function to print abc 10x
+ * print_alphabet_x10 - function to print abc 10 times
  *
- *Description: This script prints letters a-z on time lines
- *
- *Return: 0
+ * Return: 0
  */
-
 void print_alphabet_x10(void)
 {
 	char c, i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		for (c = 'a'; c <= 'z'; c++)
 		{
