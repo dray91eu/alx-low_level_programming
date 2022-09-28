33e88375fc884671b5461554fd8172bd03c2175b
dray91eu <dray91eu@gmail.com>
2022-09-28 15:22:26 -0700

    added a main.h file

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,10 +1,16 @@
-0. int _putchar(char c)
-1. void _puts_recursion(char *s);
-2. void _print_rev_recursion(char *s);
-3. int _strlen_recursion(char *s);
-4. int factorial(int n);
-5. int _pow_recursion(int x, int y);
-6. int _sqrt_recursion(int n);
-7. int is_prime_number(int n);
-8. int is_palindrome(char *s);
-9. int wildcmp(char *s1, char *s2);
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+
+#endif
